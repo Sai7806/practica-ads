3e5e5cda461701ee03cb440c0206240adfdd4f27
sai <simonvargassolis7@gmail.com>
2026-02-03 22:44:27 -0500

    pruevitas en python ajajja

---
+++ saludo.py
@@ -0,0 +1,10 @@
+nombre = input("¿Cómo te llamas? bro ")
+print(f" hola {nombre}, lo estamos logrando ")
+edad = input("¿Cuántos años tienes? bro ")
+
+if edad == "17":
+    print("tienes mi misma edad bro ajajja")
+elif edad < "17":
+    print("mas pequeño que yo pero bueno mano jajajaj")
+else:
+    print("mero viejo jajajjaj")
